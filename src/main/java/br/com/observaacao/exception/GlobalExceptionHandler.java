bcb9fce4235b92a3dfdd150762252e22d1971b58
Leonardo Camilotti Moreno <leonardocamilottimoreno@gmail.com>
2026-06-09 03:44:51 +0000

    fix: handle HttpMessageNotReadableException in GlobalExceptionHandler

--- src/main/java/br/com/observaacao/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/observaacao/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package br.com.observaacao.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -26,5 +27,11 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Map<String, String>> handleValidacao(ValidacaoException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
             .body(Map.of("erro", ex.getMessage()));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Map<String, String>> handleJsonInvalido(HttpMessageNotReadableException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(Map.of("erro", "Valor inválido no corpo da requisição. Verifique os tipos e enums enviados."));
     }
 }
